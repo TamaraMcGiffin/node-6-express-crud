f17914bee21f24b59f226ccaec8fb903fd47241c
tamaramcgiffin <tamaramcgiffin@gmail.com>
2025-11-09 14:47:55 -0800

    Recipes API: Boilerplate, Helper, & GET functions

--- books-api/index.js
+++ books-api/index.js
@@ -2,18 +2,115 @@
 // Boilerplate Code to Set Up Server
 // ---------------------------------
 
+//Boiler plate code to start server:
+//Importing all of our node modules
+import express from "express"; // the framework that lets us build webservers
+
+import fs from "fs/promises";
+
+//Declare a variable named app and call the express() function to create a new instance of express so we can use all of the methods, fucntions, properties of express
+// which will be saved in app
+const app = express();
+
+//Defining out port number
+//What port should our server listen to?
+const port = 3000; // you can use any port # but developers commonly use 3000. also there are some port numbers you cannot use
+
+//Declaring that this server will be receiving and responding to requests in JSON
+app.use(express.json());
+
+//Turn on our server so that it can listen for requests and respond to those requests at our port #
+//Hello you are on , listen to requests and respond to those requests
+app.listen(port, () => {
+  console.log(`Server is listening on port #${port}`);
+}); //this method is turning on our server
+
+// We will create the beginnings of a CRUD application
+// CRUD stands for Create Read Update Delete
+
 // ---------------------------------
 // Helper Functions
 // ---------------------------------
 
-// 1. getAllBooks()
+// 1. GET getAllBooks()
+
+async function getAllBooks() {
+  const data = await fs.readFile("./books-data.JSON", "utf8");
+  const parsedBooks = JSON.parse(data);
+  return parsedBooks;
+}
 
 // 2. getOneBook(index)
 
+async function getOneBook(index) {
+  // Read the books data from the books-data.json file
+  const data = await fs.readFile("./books-data.JSON", "utf8");
+
+  // Parse the books data to turn it from JSON to Javascript format
+  const parsedBooks = JSON.parse(data);
+
+  // Return the data we're looking for
+  return parsedBooks[index];
+}
+// 3. getOneBookTitle(index)
+
+async function getOneBookTitle(index) {
+  //read the books data from the books-data.json file
+  const data = await fs.readFile("./books-data.JSON", "utf8");
+  //parse the books data to turn it from JSON to Javascript
+  const parsedBooks = JSON.parse(data);
+  //return the data we're looking for: the title of the book at the specified index
+  return parsedBooks[index].title;
+}
 // ---------------------------------
 // API Endpoints
 // ---------------------------------
 
 // 1. GET /get-all-books
 
+app.get("/get-all-books", async (req, res) => {
+  // Call the helper function and save it's return value in a variable
+  const books = await getAllBooks();
+
+  // We want to read data from the books-data.json file
+  // The fs.readFile() method takes in 2 parameter:
+  // 1. The file path to the file we want to read from
+  // 2. The encoding
+
+  // This code copied over to function above for refactoring
+  //   const data = await fs.readFile("./books-data.JSON", "utf8");
+  //   const parsedBooks = JSON.parse(data);
+
+  // res.send() sends a response in text data in the response
+  // res.json() sends JSON data in the response
+  res.json(books);
+});
+
 // 2. GET /get-one-book/:index
+
+app.get("/get-one-book/:index", async (req, res) => {
+  // Get the value of the index dynamic parameter
+  const index = req.params.index;
+
+  // call the helper function that gets the book from the file
+  const book = await getOneBook(index);
+
+  // send the book as JSON data in the response
+  res.json(book);
+});
+
+// 3. GET /get-one-book-title/:index
+
+app.get("/get-one-book-title/:index", async (req, res) => {
+  // get the value of the index dynamic parameter
+  const index = req.params.index;
+
+  //call the helper function that gets the book's title at the specified index
+  const bookTitle = await getOneBookTitle(index);
+
+  // Send the response as text - Alternative #1
+  //   res.send(bookTitle);
+
+  // Alternative #2
+  res.json({ title: bookTitle });
+});
--- recipes-api/index.js
+++ recipes-api/index.js
@@ -2,17 +2,69 @@
 // Boilerplate Code to Set Up Server
 // ---------------------------------
 
+//Boiler plate code to start server:
+//Importing all of our node modules
+import express from "express"; // the framework that lets us build webservers
+
+import fs from "fs/promises";
+
+//Declare a variable named app and call the express() function to create a new instance of express so we can use all of the methods, fucntions, properties of express
+// which will be saved in app
+const app = express();
+
+//Defining out port number
+//What port should our server listen to?
+const port = 3000; // you can use any port # but developers commonly use 3000. also there are some port numbers you cannot use
+
+//Declaring that this server will be receiving and responding to requests in JSON
+app.use(express.json());
+
+//Turn on our server so that it can listen for requests and respond to those requests at our port #
+//Hello you are on , listen to requests and respond to those requests
+app.listen(port, () => {
+  console.log(`Server is listening on port #${port}`);
+}); //this method is turning on our server
+
 // ---------------------------------
 // Helper Functions
 // ---------------------------------
 
 // 1. getAllRecipes()
 
+async function getAllRecipes() {
+  const data = await fs.readFile("./recipes-data.JSON", "utf8");
+  const parsedRecipes = JSON.parse(data);
+  return parsedRecipes;
+}
+
 // 2. getOneRecipe(index)
+
+async function getOneRecipe(index) {
+  // Read the books data from the books-data.json file
+  const data = await fs.readFile("./recipes-data.JSON", "utf8");
+
+  // Parse the books data to turn it from JSON to Javascript format
+  const parsedRecipes = JSON.parse(data);
+
+  // Return the data we're looking for
+  return parsedRecipes[index];
+}
 
 // 3. getAllRecipeNames()
 
+async function getAllRecipeNames() {
+  const data = await fs.readFile("./recipes-data.JSON", "utf8");
+  const parsedRecipes = JSON.parse(data);
+  //return the data we're looking for: the name of the recipe at the specified index
+  return parsedRecipes.map((recipe) => recipe.name);
+}
+
 // 4. getRecipesCount()
+
+async function getRecipesCount() {
+  const recipes = await getAllRecipes();
+  return recipes.length;
+}
 
 // ---------------------------------
 // API Endpoints
@@ -20,8 +72,31 @@
 
 // 1. GET /get-all-recipes
 
+app.get("/get-all-recipes", async (req, res) => {
+  // Call the helper function
+  const recipes = await getAllRecipes();
+
+  res.json(recipes);
+});
+
 // 2. GET /get-one-recipe/:index
+
+app.get("/get-one-recipe/:index", async (req, res) => {
+  const index = req.params.index;
+  const recipe = await getOneRecipe(index);
+  res.json(recipe);
+});
 
 // 3. GET /get-all-recipe-names
 
+app.get("/get-all-recipe-names", async (req, res) => {
+  const recipeNames = await getAllRecipeNames();
+  res.json(recipeNames);
+});
+
 // 4. GET /get-recipes-count
+
+app.get("/get-recipe-count", async (req, res) => {
+  const count = await getRecipesCount();
+  res.json({ recipesCount: count });
+});
